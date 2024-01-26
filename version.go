790e951ca819487ee00b95eef303e8a0193a66ea
cluttrdev <andreas@cluttr.tech>
2024-01-26 10:01:02 +0100

    feat: Allow overriding build info version number

--- version.go
+++ version.go
@@ -29,16 +29,32 @@ type VersionInfo interface {
 	GoVersion() string
 }
 
-type buildInfo struct {
-	debug.BuildInfo
+type BuildInfo struct {
+	buildInfo *debug.BuildInfo
+	version   string
 }
 
-func (i *buildInfo) Version() string {
-	return i.BuildInfo.Main.Version
+func NewBuildInfo(version string) *BuildInfo {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		info = &debug.BuildInfo{}
+	}
+
+	return &BuildInfo{
+		buildInfo: info,
+		version:   version,
+	}
 }
 
-func (i *buildInfo) Revision() string {
-	for _, setting := range i.BuildInfo.Settings {
+func (bi *BuildInfo) Version() string {
+	if bi.version != "" {
+		return bi.version
+	}
+	return bi.buildInfo.Main.Version
+}
+
+func (bi *BuildInfo) Revision() string {
+	for _, setting := range bi.buildInfo.Settings {
 		if setting.Key == "vcs.revision" {
 			return setting.Value
 		}
@@ -46,8 +62,8 @@ func (i *buildInfo) Revision() string {
 	return ""
 }
 
-func (i *buildInfo) Time() string {
-	for _, setting := range i.BuildInfo.Settings {
+func (bi *BuildInfo) Time() string {
+	for _, setting := range bi.buildInfo.Settings {
 		if setting.Key == "vcs.time" {
 			return setting.Value
 		}
@@ -55,8 +71,8 @@ func (i *buildInfo) Time() string {
 	return ""
 }
 
-func (i *buildInfo) Modified() bool {
-	for _, setting := range i.BuildInfo.Settings {
+func (bi *BuildInfo) Modified() bool {
+	for _, setting := range bi.buildInfo.Settings {
 		if setting.Key == "vcs.modified" {
 			v, err := strconv.ParseBool(setting.Value)
 			if err != nil {
@@ -68,8 +84,8 @@ func (i *buildInfo) Modified() bool {
 	return false
 }
 
-func (i *buildInfo) GoVersion() string {
-	return i.BuildInfo.GoVersion
+func (bi *BuildInfo) GoVersion() string {
+	return bi.buildInfo.GoVersion
 }
 
 func DefaultVersionInfo() VersionInfo {
@@ -78,20 +94,20 @@ func DefaultVersionInfo() VersionInfo {
 		info = &debug.BuildInfo{}
 	}
 
-	return &buildInfo{
-		BuildInfo: *info,
+	return &BuildInfo{
+		buildInfo: info,
 	}
 }
 
-func DefaultVersionCommand(stdout io.Writer) *Command {
+func NewVersionCommand(info VersionInfo, out io.Writer) *Command {
 	cfg := versionCmdConfig{
-		version: DefaultVersionInfo(),
+		version: info,
 		flags:   flag.NewFlagSet("version", flag.ExitOnError),
-		stdout:  stdout,
+		out:     out,
 	}
 
-	if cfg.stdout == nil {
-		cfg.stdout = os.Stdout
+	if cfg.out == nil {
+		cfg.out = os.Stdout
 	}
 
 	cfg.RegisterFlags(cfg.flags)
@@ -104,12 +120,17 @@ func DefaultVersionCommand(stdout io.Writer) *Command {
 	}
 }
 
+func DefaultVersionCommand(out io.Writer) *Command {
+	info := DefaultVersionInfo()
+	return NewVersionCommand(info, out)
+}
+
 type versionCmdConfig struct {
 	version VersionInfo
 
 	flags *flag.FlagSet
 
-	stdout io.Writer
+	out io.Writer
 }
 
 func (c *versionCmdConfig) RegisterFlags(fs *flag.FlagSet) {
@@ -130,6 +151,21 @@ func (c *versionCmdConfig) RegisterFlags(fs *flag.FlagSet) {
 	fs.Bool("json", false, "print information in JSON")
 }
 
+func (c *versionCmdConfig) Exec(ctx context.Context, args []string) error {
+	any := false
+	c.flags.Visit(func(f *flag.Flag) {
+		if f.Name != "json" {
+			any = true
+		}
+	})
+	all := testFlag(c.flags, "all")
+
+	if testFlag(c.flags, "json") {
+		return c.writeJson(any, all)
+	}
+	return c.writeText(any, all)
+}
+
 func testFlag(fs *flag.FlagSet, name string) bool {
 	f := fs.Lookup(name)
 	if f == nil {
@@ -144,23 +180,7 @@ func testFlag(fs *flag.FlagSet, name string) bool {
 	return v
 }
 
-func (c *versionCmdConfig) Exec(ctx context.Context, args []string) error {
-	if testFlag(c.flags, "json") {
-		return c.writeJson()
-	}
-	return c.writeText()
-}
-
-func (c *versionCmdConfig) writeText() error {
-	any := false
-	c.flags.Visit(func(f *flag.Flag) {
-		if f.Name != "number" {
-			any = true
-		}
-	})
-
-	all := testFlag(c.flags, "all")
-
+func (c *versionCmdConfig) writeText(any bool, all bool) error {
 	builder := strings.Builder{}
 
 	if !any || testFlag(c.flags, "number") || all {
@@ -183,20 +203,30 @@ func (c *versionCmdConfig) writeText() error {
 
 	s := builder.String()
 
-	_, err := fmt.Fprintln(c.stdout, strings.TrimSpace(s))
+	_, err := fmt.Fprintln(c.out, strings.TrimSpace(s))
 	if err != nil {
 		return fmt.Errorf("error writing version information: %w", err)
 	}
 	return nil
 }
 
-func (c *versionCmdConfig) writeJson() error {
-	data := map[string]string{
-		"Version":   c.version.Version(),
-		"Revision":  c.version.Revision(),
-		"Time":      c.version.Time(),
-		"GoVersion": c.version.GoVersion(),
-		"Modified":  fmt.Sprint(c.version.Modified()),
+func (c *versionCmdConfig) writeJson(any bool, all bool) error {
+	data := map[string]string{}
+
+	if !any || testFlag(c.flags, "number") || all {
+		data["Version"] = c.version.Version()
+	}
+	if testFlag(c.flags, "revision") || all {
+		data["Revision"] = c.version.Revision()
+	}
+	if testFlag(c.flags, "time") || all {
+		data["Time"] = c.version.Time()
+	}
+	if testFlag(c.flags, "go-version") || all {
+		data["GoVersion"] = c.version.GoVersion()
+	}
+	if testFlag(c.flags, "modified") || all {
+		data["Modified"] = fmt.Sprint(c.version.Modified())
 	}
 
 	m, err := json.Marshal(data)
@@ -204,7 +234,7 @@ func (c *versionCmdConfig) writeJson() error {
 		return fmt.Errorf("error encoding version information: %w", err)
 	}
 
-	_, err = fmt.Fprintln(c.stdout, string(m))
+	_, err = fmt.Fprintln(c.out, string(m))
 	if err != nil {
 		return fmt.Errorf("error writing version information: %w", err)
 	}
